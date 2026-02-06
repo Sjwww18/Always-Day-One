0bbd15344949972a25c489ed19b5c1b41ede4c31
SJW <jiaweis981@gmail.com>
2026-02-06 16:56:53 +0800

    new file for main logic of app/eval.py

---
+++ app/core/evaluating.py
@@ -0,0 +1,51 @@
+# app/core/evaluating.py
+
+import numpy as np
+from tqdm import tqdm
+from typing import Any, List, Tuple
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+from app.core.logger import setup_logger
+logger = setup_logger(__name__)
+
+
+class Evaluator:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        loss_fn: torch.nn.Module,
+        eval_loader: Any,
+        device: torch.device,
+        writer: SummaryWriter=None
+    ):
+        self.Model = model
+        self.Loss = loss_fn
+        self.EvalLoader = eval_loader
+        self.Device = device
+        self.Writer = writer
+    
+    def evaluating(self) -> List[Tuple[Any, np.ndarray]]:
+        self.Model.eval()
+
+        with torch.no_grad():
+            Result = [None] * len(self.EvalLoader)
+            for i, (key, X, y, mask) in enumerate(
+                tqdm(self.EvalLoader, desc="评估日期进度")
+            ):
+                X = torch.from_numpy(X).to(self.Device)
+                # y = torch.from_numpy(y).to(self.Device)
+                # if mask is not None:
+                #     mask = torch.from_numpy(mask).to(self.Device)
+
+                ypre = self.Model(X)
+                if isinstance(ypre, torch.Tensor):
+                    ypre = ypre.cpu().numpy()
+                ypre = self.EvalLoader.process(ypre)
+                Result[i] = (key, ypre)
+
+        return Result
+
+
+# end of app/core/evaluating.py
